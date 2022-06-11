02ed78da0ce7b70079557a88ef2c79e9c6f49eb5
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-11 23:33:05 +0100

    buttons

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ const author = document.querySelector('.quote-author');
 
 //all the buttons
 const newQuoteBtn = document.querySelector('.new-quote-btn');
-const facbookShareBtn = document.querySelector('.facebook-share-btn');
+const tweetBtn = document.querySelector('.tweet-btn');
 const speechBtn = document.querySelector('.speech-btn');
 const copyBtn = document.querySelector('.copy-btn');
 
 newQuoteBtn.addEventListener('click', getRandomQuote);
-facbookShareBtn.addEventListener('click', shareToFacebook);
+tweetBtn.addEventListener('click', tweeQuote);
 speechBtn.addEventListener('click', quoteToSpeech);
 copyBtn.addEventListener('click', copyQuote);
 
@@ -28,18 +28,23 @@ function getRandomQuote(){
 }
 
 //share the quote to facboook
-function shareToFacebook(){
-    console.log("");
+function tweeQuote(){
+    const text = quote.textContent;
+    const url = "https://www.twitter.com/intent/tweet?text=" + text + " By " + author.textContent;
+    window.open(url, '_blank');
 }
 
 //generate the speech of the quote
 function quoteToSpeech(){
-    console.log("");
+    const text = quote.textContent;
+    const utterance = new SpeechSynthesisUtterance(text + " by " + author.textContent);
+    utterance.lang = 'en-US';
+    speechSynthesis.speak(utterance);
 }
 
-//copy the quote to the clipbord
+//copy the quote to the clipboard
 function copyQuote(){
     //copy the quote to the clipboard
     const text = quote.textContent;
-
+    navigator.clipboard.writeText(text);
 }
