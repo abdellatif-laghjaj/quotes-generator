26620488d8b29a726f071d7d1eccef8cb2ba7726
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-11 22:35:47 +0100

    get random quote

--- js/main.js
+++ js/main.js
@@ -18,7 +18,8 @@ function getRandomQuote(){
     fetch("https://api.quotable.io/random")
     .then(res => res.json())
     .then(result => {
-        console.log(result);
+        quote.textContent = result.content;
+        author.textContent = result.author;
     });
 }
 
