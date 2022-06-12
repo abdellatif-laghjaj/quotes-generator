6de629304718b10d209d2a1e00092fbb829e8156
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-06-12 01:18:55 +0100

    full theming

--- js/main.js
+++ js/main.js
@@ -62,16 +62,15 @@ function copyQuote(){
 
 //color palette
 const colors = [
-    '#e6194b',
-    '#3cb44b',
-    '#ffe119',
-    '#4363d8',
-    '#f58231',
-    '#911eb4',
-    '#46f0f0',
-    '#bcf60c',
-    '#fabebe',
-    '#008080',
+    'hsl(345, 80%, 50%)',
+    'hsl(100, 80%, 50%)',
+    'hsl(200, 80%, 50%)',
+    'hsl(227, 66%, 55%)',
+    'hsl(26, 80%, 50%)',
+    'hsl(44, 90%, 51%)',
+    'hsl(280, 100%, 65%)',
+    'hsl(480, 100%, 25%)',
+    'hsl(180, 100%, 25%)',
 ];
 
 const colorBtns = document.querySelectorAll('.theme-color');
